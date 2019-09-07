55797c11e1ab275f9198a01db99fc190501ae4f9
lifelmy <1298639127@qq.com>
2019-09-07 14:16:20 +0800

    'test'

---
+++ src/test/Main2.java
@@ -0,0 +1,27 @@
+package test;
+
+
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        TreeSet<Integer> set=new TreeSet<>();
+        
+        int count=scanner.nextInt();
+        for (int i = 0; i <count ; i++) {
+            int temp=scanner.nextInt();
+            if(!set.contains(temp)){
+                set.add(temp);
+            }
+        }
+
+        while (!set.isEmpty()){
+            System.out.println( set.pollFirst());
+        }
+        
+        
+    }
+}
