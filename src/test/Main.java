bde251817d4f5a1d4bba6fc3786954f13af908cb
lifelmy <1298639127@qq.com>
2019-09-07 14:09:14 +0800

    'test'

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,12 +1,36 @@
 package test;
 
-/**
- * @author lmy
- * @ClassName Main
- * @date 2019/9/7
- **/
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner=new Scanner(System.in);
+        while (scanner.hasNext()){
+            int num=scanner.nextInt();
+            if(num==0){
+                return;
+            }
+            System.out.println(process(num));
+        }
     }
+
+
+    public static int process(int num){
+
+        int left=num;
+        int total=0;
+        while (left>2){
+            int temp=left/3;
+            total+=temp;
+            left=left-temp*3+temp;
+        }
+
+        if(left==2){
+            total+=1;
+        }
+        return total;
+    }
+
+
 }
