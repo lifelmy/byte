01c8448744b38018cdcf0ff4bf9b9e94bd162105
lifelmy <1298639127@qq.com>
2019-09-07 14:47:24 +0800

    'test'

--- src/test/Main3.java
+++ src/test/Main3.java
@@ -4,5 +4,27 @@ package test;
 public class Main3 {
     public static void main(String[] args) {
 
+
+        System.out.println(process("0xA"));
+
+
+
     }
+
+    public static int process(String str){
+        String input=str.substring(2);
+        int total=0;
+
+        for (int i = 0; i <input.length() ; i++) {
+            char temp=input.charAt(i);
+            if(temp>='0'&&temp<='9'){
+                total=total*16+temp-'0';
+            }else {
+                total=total*16+temp-'A'+10;
+            }
+        }
+        return total;
+    }
+
+
 }
